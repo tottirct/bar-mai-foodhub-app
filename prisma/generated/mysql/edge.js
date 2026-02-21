400f13ae09f8a589acec23139c0340b82228b142
Supasek Sangket <supasek.s@ku.th>
2026-02-21 21:26:28 +0700

    API For Top Up / check credit

--- prisma/generated/mysql/edge.js
+++ prisma/generated/mysql/edge.js
@@ -216,6 +216,7 @@ const config = {
     "db"
   ],
   "activeProvider": "mysql",
+  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
--- prisma/generated/mysql/index.js
+++ prisma/generated/mysql/index.js
@@ -217,6 +217,7 @@ const config = {
     "db"
   ],
   "activeProvider": "mysql",
+  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
