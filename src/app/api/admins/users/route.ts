89e666ad4c820da43c10e3ea1243830a33b45964
Supasek Sangket <supasek.s@ku.th>
2026-03-03 10:50:13 +0700

    API ข้อมูลลูกค้าคร่าวๆ return

--- src/app/api/admins/users/route.ts
+++ src/app/api/admins/users/route.ts
@@ -1,2 +1,45 @@
 //ไว้ดึงขึ้อมูบ user คร่าวๆ ทุกคน
 
+import { NextResponse, NextRequest } from 'next/server';
+import { prisma } from '@/lib/prisma';
+
+export async function GET(
+    request: Request,
+) {
+    try {
+        const users = await prisma.user.findMany({
+            select: {
+                id: true,
+                email: true,
+                name: true,
+                username: true,
+                role: true,
+                isBanned: true,
+                wallets: {
+                    select: {
+                        balance: true
+                    }
+                }
+            }
+        });
+
+        const formattedUserData = users.map((user) => {
+            return {
+                id: user.id,
+                email: user.email,
+                name: user.name,
+                username: user.username,
+                role: user.role,
+                isBanned: user.isBanned,
+                balance: user.wallets?.balance || null
+            }
+        });
+
+        return NextResponse.json({
+            success: true,
+            data: formattedUserData
+        })
+    } catch(error) {
+
+    }
+}
