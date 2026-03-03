97f0adc22f58c2ee9fa80a600229db7732bfabf4
Supasek Sangket <supasek.s@ku.th>
2026-03-03 18:05:54 +0700

    API init

--- src/app/api/admins/users/[userid]/transactions/route.ts
+++ src/app/api/admins/users/[userid]/transactions/route.ts
@@ -1,1 +1,15 @@
 // ไว้ดึง transaction user ไม่ส่งไปหมดทีเดียวเดี๋ยว lag
+import { NextResponse, NextRequest } from 'next/server'
+import { prisma } from '@/lib/prisma'
+import { mongo } from '@/lib/mongo'
+
+export async function PATCH(
+    request: Request,
+    { params }: { params : Promise<{ userid : String }>}
+) {
+    try {
+
+    } catch(error) {
+        
+    }
+}
